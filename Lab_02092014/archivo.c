67ddb3a732b6f2e20c31d3b00da71e0846313b09
Joaquin villagra Pacheco <villagra.pacheco.joaquin@gmail.com>
2014-09-06 14:28:26 -0400

    Cambios basicos

--- Lab_02092014/archivo.c
+++ Lab_02092014/archivo.c
@@ -5,19 +5,16 @@ int main()
 {
 
 	//Definición de variables
-	char nombre[50];
-	char cod_carrera[4];
-	int edad;
-	int a_ingreso;
+	Alumno *alumnos;
 	//Crear arreglo donde se guardaran los strings entrantes
 	char lectura[100];
 	char** array_lectura;
 	//Crear puntero donde se guardará la dirección del archivo
 	FILE * archivo;
 	//Abrir el archvio nombre.txt en modo de lectura read
-	archivo = fopen("ListaAlumnos.txt", 'r');
+	archivo = fopen("ListaAlumnos.txt", "r");
 	//si el archivo existe será distinto de null
-	if(archivo!=null)
+	if(archivo==NULL)
 	{
 		printf("El archivo ListaAlumnos.txt no existe, tiene un nombre diferente o no se tiene permiso de lectura.\n");
 	}
@@ -34,23 +31,31 @@ int main()
 				//Definición de Variables
 				int cantidad_alumnos = atoi(lectura);
 				//Se crea arreglo de alumnos del largo total existente.
-				Alumno *alumnos = (Alumno *)malloc(sizeof(Alumno)*cantidad_alumnos);
+				alumnos = (Alumno *)malloc(sizeof(Alumno)*cantidad_alumnos);
 			}
 			else 
 			{
-				contador++;
-				fgets(lectura,100,archivo);
+				
+				fgets(lectura, 100, archivo);
+				//printf("lectura: %s", lectura);
 				array_lectura = str_split(lectura, ',');
-				*(alumnos[indice]->nombre)      = array_lectura[1];
-				*(alumnos[indice]->edad)        = atoi(array_lectura[2]);
-				*(alumnos[indice]->cod_carrera) = array_lectura[3];
-				*(alumnos[indice]->a_ingreso)   = atoi(array_lectura[4]);
+				//printf("%d\n", contador );
+				//printf("%s", array_lectura[0]);
+				//printf("%s", array_lectura[1]);
+				//printf("%s", array_lectura[2]);
+				//printf("%s", array_lectura[3]);
+
+				strcpy(alumnos[indice].nombre, array_lectura[0]);
+				alumnos[indice].edad = atoi(array_lectura[1]);
+				strcpy(alumnos[indice].cod_carrera, array_lectura[2]);
+				alumnos[indice].a_ingreso = atoi(array_lectura[3]);
+				printf("Guardé la linea %d\n", contador);
+				printf("Nombre del alumno numero %d: %s\n", (indice+1), alumnos[indice].nombre);
+				contador++;
+				indice++;
 			}
 		} 
 		//Finaliza While y se completa el arreglo con "cantidad_alumnos" elementos.
 	}
-
-	
-	getche();
 	return 0;
 }
