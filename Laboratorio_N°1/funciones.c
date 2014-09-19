9b9f58ed09a46e546eb801e76a3f489feda94b0a
Joaquin villagra Pacheco <villagra.pacheco.joaquin@gmail.com>
2014-09-18 21:27:21 -0300

    Agregados Informe & Imagenes

---
+++ Laboratorio_N°1/funciones.c
@@ -0,0 +1,64 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define LARGO 1500
+
+int *sumaDeValores(int numero1[], int numero2[], int largo)
+{
+    int indice, acumulador=0;
+    int *Array = (int*)malloc(sizeof(int)*largo);
+    for(indice=largo-1; indice>=0; indice--)
+    {
+        int s = numero1[indice] + numero2[indice];
+        if(s/10>=1)
+        {
+            Array[indice]     = s % 10;
+            acumulador        = s / 10;
+            numero1[indice-1] = numero1[indice-1] + acumulador;
+        }
+        else
+        {
+            Array[indice] = s;
+        }
+    }
+    return Array;
+}
+
+
+void fibonacci(int n)
+{
+    int Valor1[LARGO], Valor2[LARGO], indiceAuxiliar, indice, valor = 1;
+    for(indice=0; indice<LARGO; indice++)
+    {
+        Valor1[indice]=0;
+        Valor2[indice]=0;
+	    if(indice==LARGO-1)
+	    {
+            Valor2[indice]=1;
+        }   
+    }
+    for(indiceAuxiliar = 1; indiceAuxiliar < n; indiceAuxiliar++)
+    {
+        int *Puntero = sumaDeValores(Valor1, Valor2, LARGO);
+        for(indice=0;indice<LARGO;indice++)
+        {
+          Valor1[indice] = Valor2[indice];
+          Valor2[indice] = Puntero[indice];
+        }
+    }
+    for(indice=0;indice<LARGO;indice++)
+    {    
+        if(valor==1)
+        {
+            if (Valor2[indice+1]!=0)
+            {
+                 valor=0;
+            }          
+        }
+        else
+        {
+            printf("%d", Valor2[indice]);
+        }
+    }    
+}
--- Laboratorio_N°1/laboratorio.c
+++ Laboratorio_N°1/laboratorio.c
@@ -1,8 +1,11 @@
-#include "funciones.h"
+
+
+#include "funciones.c"
 
 int main(int argc, char **argv)
 {
     int n = atoi(argv[1]);
     fibonacci(n);
+    printf("\n");
     return 0;
 }
